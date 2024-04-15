7f9e516f8812c4a44e72993a8b22589312d78b17
Raj V Singh <118731884+rajsingh162120@users.noreply.github.com>
2024-04-15 20:59:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -357,4 +357,14 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+hide_st_style="""
+                  <style>
+                  #MainMenu {visibility: hidden;}
+                  footer {visibility: hidden;}
+                  header {visibility: hidden;}
+                  </style>
+                  """
+    
+st.markdown(hide_st_style,unsafe_allow_html=True)
+
 display_page()
