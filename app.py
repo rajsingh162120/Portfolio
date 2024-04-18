61be0b624af9c50687ade42343efd5bc4a531ddf
Raj V Singh <118731884+rajsingh162120@users.noreply.github.com>
2024-04-18 07:26:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def display_projects():
             "images": ["Project-image/smart-grid-1.png", "Project-image/smart-grid-2.png"],  # List of paths to your project images
             "description": "Our team developed a sophisticated Smart Grid prediction model using machine learning algorithms such as Random Forest Regression and Gradient Boosting. This model effectively predicts grid behavior, contributing to optimized energy management and resource allocation. We also make a website for the user input of air pressure , Temperature and wind speed to get the power generated . Also , we have integrated the power bi dashboard for the analysis of last 5 year data.",
             "github": "https://github.com/UtkarshTiwari1750/9_Sustainability_and_Environment",
-            "Linkedin": "https://www.linkedin.com/posts/raj01singh_hackathon-smartgrid-machinelearning-activity-7186431027311951873-zO7-?utm_source=share&utm_medium=member_android",
+            "linkedin": "https://www.linkedin.com/posts/raj01singh_hackathon-smartgrid-machinelearning-activity-7186431027311951873-zO7-?utm_source=share&utm_medium=member_android",
              
         },
         {
